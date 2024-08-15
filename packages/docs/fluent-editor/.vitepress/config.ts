147c91348cc1604bf0508f8d5f794a581649c4be
Kagol <kagol@sina.com>
2024-08-15 10:46:21 +0800

    docs: optimize docs (#6)

--- packages/docs/fluent-editor/.vitepress/config.ts
+++ packages/docs/fluent-editor/.vitepress/config.ts
@@ -4,11 +4,16 @@ import { demoPreviewPlugin } from '@vitepress-code-preview/plugin'
 import { sidebar } from './sidebar'
 
 export default defineConfig({
-  title: 'FluentEditor',
-  description: '富文本编辑器, Rich text editor, rich-text-editor, rich-text, wysiwyg, wysiwyg-editor, fluent-editor',
+  title: 'Fluent Editor',
+  titleTemplate: '基于 Quill 2.0 的富文本编辑器',
+  description: '富文本编辑器, Rich text editor, rich-text-editor, rich-text, wysiwyg, wysiwyg-editor, quill, fluent-editor',
   base: '/fluent-editor/',
   cleanUrls: true,
+  head: [
+    ['link', { rel: 'icon', href: 'favicon.ico' }]
+  ],
   themeConfig: {
+    logo: 'logo.svg',
     nav: [
       { text: '文档', link: '/docs/quick-start', activeMatch: '/docs/' },
       {
@@ -23,6 +28,45 @@ export default defineConfig({
     ],
     sidebar: {
       '/docs/': sidebar(),
+    },
+    footer: {
+      message: 'Made with ❤ by',
+      copyright: '<a href="https://opentiny.design/" target="_blank">OpenTiny</a> and his friends'
+    },
+    outline: {
+      label: '本页目录'
+    },
+    sidebarMenuLabel: '菜单',
+    returnToTopLabel: '返回顶部',
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    },
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索'
+          },
+          modal: {
+            displayDetails: '显示详情',
+            resetButtonTitle: '重置',
+            backButtonTitle: '关闭搜索',
+            noResultsText: '无相关结果',
+            footer: {
+              selectText: '选择',
+              selectKeyAriaLabel: 'Enter',
+              navigateText: '切换',
+              navigateUpKeyAriaLabel: '向上箭头',
+              navigateDownKeyAriaLabel: '向下箭头',
+              closeText: '关闭',
+              closeKeyAriaLabel: 'ESC'
+            }
+          }
+        }
+      }
     }
   },
   markdown: {
