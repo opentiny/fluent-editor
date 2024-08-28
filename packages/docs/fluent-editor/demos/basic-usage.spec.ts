919a861a7d9744b2b05563cc53f9a8d22895311e
Kagol <kagol@sina.com>
2024-08-28 22:20:15 +0800

    test: optimize basic-usage test (#29)

--- packages/docs/fluent-editor/demos/basic-usage.spec.ts
+++ packages/docs/fluent-editor/demos/basic-usage.spec.ts
@@ -1,10 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('has toolbar', async ({ page }) => {
-  page.on('pageerror', (exception) => expect(exception).toBeNull())
+  page.on('pageerror', (exception) => expect(exception).toBeNull());
   await page.goto('http://localhost:5173/fluent-editor/docs/basic-usage');
 
-  const toolbar = page.locator('.ql-toolbar')
+  const toolbar = page.locator('.ql-toolbar');
+  const editor = page.locator('.ql-editor');
 
   await expect(toolbar).toBeVisible();
+  await expect(await editor.innerHTML()).toEqual(
+    [
+      '<p>',
+      'Hello ',
+      `<strong>Fluent Editor</strong>`,
+      '!',
+      `</p>`,
+    ].join(''),
+  );
 });
