896f68ce2eb56241c66ed4a48141fcba3aac4a81
zzxming <74341337+zzxming@users.noreply.github.com>
2024-09-09 14:42:24 +0800

    fix: format-painter with not format warning (#48)

--- packages/fluent-editor/src/format-painter/index.ts
+++ packages/fluent-editor/src/format-painter/index.ts
@@ -1,79 +1,79 @@
-import Quill from 'quill';
-import type { Range } from 'quill';
-import type Toolbar from 'quill/modules/toolbar';
+import Quill from 'quill'
+import type { Range } from 'quill'
+import type Toolbar from 'quill/modules/toolbar'
 
-export class FormatPainter {
-  static toolName = 'format-painter';
-  static moduleName = 'format-painter';
-
-  formatterBtn: HTMLElement | null;
-  isFormatterLock = false;
-  isFormating = false;
-  rangeFormat: Record<string,any> = {}
-
-  constructor(public quill: Quill) {
-    this.quill = quill;
-
-    const toolbar = this.quill.getModule('toolbar') as Toolbar;
-    this.formatterBtn = toolbar.container.querySelector('.ql-format-painter');
-    if (toolbar && this.formatterBtn) {
-      this.formatterBtn.addEventListener('dblclick', () => {
-        if (this.isFormatterLock) {
-          this.unbindFormatSelect();
-          return;
-        }
-        this.isFormatterLock = true;
-        this.bindFormatSelect();
-      });
-      this.formatterBtn.addEventListener('click', () => {
-        if (!this.isFormating) {
-          this.bindFormatSelect();
-        } else {
-          this.unbindFormatSelect();
-        }
-      });
+type FormatData = {
+  formatPainter: {
+    rangeFormat: Record<string, any>
+    isFormatterLock: boolean
+    isFormating: boolean
+    prepareLock: boolean
+  }
+}
+export function FormatPainter(this: Toolbar & FormatData) {
+  if (!this.formatPainter) {
+    this.formatPainter = {
+      rangeFormat: {},
+      isFormatterLock: false,
+      isFormating: false,
+      prepareLock: false,
     }
   }
-
-  btnActive = () => {
-    setTimeout(() => {
-      this.formatterBtn.classList.add('ql-active');
-    }, 0);
-  };
-
-  btnRemoveActive = () => {
-    setTimeout(() => {
-      this.formatterBtn.classList.remove('ql-active');
-    }, 0);
-  };
-
-  bindFormatSelect = () => {
-    const range = this.quill.getSelection();
-    if (range.length === 0) return;
-    this.rangeFormat = this.quill.getFormat(range);
-    this.isFormating = true;
-    this.quill.on(Quill.events.SELECTION_CHANGE, this.formatRange);
-    this.btnActive();
-  };
-
-  unbindFormatSelect = () => {
-    this.quill.off(Quill.events.SELECTION_CHANGE, this.formatRange);
-    this.rangeFormat = undefined;
-    this.btnRemoveActive();
-    this.isFormating = false;
-    this.isFormatterLock = false;
-  };
-
-  formatRange = (range: Range | null) => {
-    if (!range) return;
-    this.quill.removeFormat(range.index, range.length);
-    for (const format in this.rangeFormat) {
-      this.quill.format(format, this.rangeFormat[format], Quill.sources.USER);
+  const [, formatterBtn] = this.controls.find(([name]) => name === FormatPainter.toolName)
+  const formatRange = (range: Range | null) => {
+    if (!range) return
+    this.quill.removeFormat(range.index, range.length)
+    for (const format in this.formatPainter.rangeFormat) {
+      this.quill.format(format, this.formatPainter.rangeFormat[format], Quill.sources.USER)
     }
-    if (!this.isFormatterLock) {
-      this.unbindFormatSelect();
-    } else {
-      this.btnActive();
+    if (!this.formatPainter.isFormatterLock) {
+      unbindFormatSelect()
     }
-  };
+    else {
+      btnActive()
+    }
+  }
+  // setTime for Toolbar.update
+  const btnActive = () => {
+    setTimeout(() => {
+      formatterBtn.classList.add('ql-active')
+    }, 0)
+  }
+  const btnRemoveActive = () => {
+    setTimeout(() => {
+      formatterBtn.classList.remove('ql-active')
+    }, 0)
+  }
+  const bindFormatSelect = () => {
+    const range = this.quill.getSelection()
+    if (range.length === 0) return
+    this.formatPainter.rangeFormat = this.quill.getFormat(range)
+    this.formatPainter.isFormating = true
+    this.quill.on(Quill.events.SELECTION_CHANGE, formatRange)
+    btnActive()
+  }
+  const unbindFormatSelect = () => {
+    this.quill.off(Quill.events.SELECTION_CHANGE, formatRange)
+    this.formatPainter.rangeFormat = undefined
+    btnRemoveActive()
+    this.formatPainter.isFormating = false
+    this.formatPainter.isFormatterLock = false
+  }
+
+  if (this.formatPainter.isFormatterLock || (this.formatPainter.isFormating && !this.formatPainter.prepareLock)) {
+    unbindFormatSelect()
+  }
+  else if (this.formatPainter.isFormating && this.formatPainter.prepareLock) {
+    this.formatPainter.isFormatterLock = true
+    btnActive()
+  }
+  else {
+    // imitate double click
+    this.formatPainter.prepareLock = true
+    setTimeout(() => {
+      this.formatPainter.prepareLock = false
+    }, 200)
+    bindFormatSelect()
+  }
 }
+FormatPainter.toolName = 'format-painter'
