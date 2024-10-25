7daeab49a271028f5d3c9afb1259dda507947389
zzxming <zzxming77@163.com>
2024-10-25 11:21:08 +0800

    fix: handle window resize update style

--- packages/fluent-editor/src/fullscreen/handler.ts
+++ packages/fluent-editor/src/fullscreen/handler.ts
@@ -2,18 +2,23 @@ import type { FluentEditorToolbar } from '../config/types'
 import { namespace } from '../config'
 
 let exitEscHandlerBindToolbar: (e: KeyboardEvent) => void
+let resizeHandlerBindToolbar: () => void
 function exitEscHandler(toolbar: FluentEditorToolbar, e: KeyboardEvent) {
   if (e.code === 'Escape') {
     exitFullscreen(toolbar)
   }
 }
+function updateToolbarHeight(toolbar: FluentEditorToolbar) {
+  const toolbatRect = toolbar.container.getBoundingClientRect()
+  toolbar.quill.container.style.setProperty(`--${namespace}-top`, `${toolbatRect.height}px`)
+}
 function intoFullscreen(toolbar: FluentEditorToolbar) {
   toolbar.quill.isFullscreen = true
   toolbar.container.classList.add('fullscreen')
   toolbar.quill.container.classList.add('fullscreen')
-  const toolbatRect = toolbar.container.getBoundingClientRect()
-  toolbar.quill.container.style.setProperty(`--${namespace}-top`, `${toolbatRect.height}px`)
   document.documentElement.classList.add('scroll--lock')
+  resizeHandlerBindToolbar()
+  window.addEventListener('resize', resizeHandlerBindToolbar)
   document.addEventListener('keydown', exitEscHandlerBindToolbar)
 }
 function exitFullscreen(toolbar: FluentEditorToolbar) {
@@ -21,6 +26,7 @@ function exitFullscreen(toolbar: FluentEditorToolbar) {
   toolbar.container.classList.remove('fullscreen')
   toolbar.quill.container.classList.remove('fullscreen')
   document.documentElement.classList.remove('scroll--lock')
+  window.removeEventListener('resize', resizeHandlerBindToolbar)
   document.removeEventListener('keydown', exitEscHandlerBindToolbar)
 }
 export function fullscreenHandler(this: FluentEditorToolbar) {
@@ -29,6 +35,7 @@ export function fullscreenHandler(this: FluentEditorToolbar) {
   }
   else {
     exitEscHandlerBindToolbar = exitEscHandler.bind(undefined, this)
+    resizeHandlerBindToolbar = updateToolbarHeight.bind(undefined, this)
     intoFullscreen(this)
   }
 }
