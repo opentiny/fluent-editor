7bb64dc1867a29095643f29ba8b87a6099cfa37c
zzxming <zzxming77@163.com>
2024-11-17 12:23:33 +0800

    fix: toolbar tip text with i18n

--- packages/fluent-editor/src/toolbar/toolbar-tip.ts
+++ packages/fluent-editor/src/toolbar/toolbar-tip.ts
@@ -4,89 +4,92 @@ import QuillToolbarTip from 'quill-toolbar-tip'
 import { ZH_CN } from '../config/i18n/zh-cn'
 
 export class ToolbarTip extends QuillToolbarTip {
+  static moduleName: string = 'toolbar-tip'
   constructor(public quill: FluentEditor, options: Partial<QuillToolbarTipOptions>) {
-    // const reulst = this.resolveOptions(options)
-    // const langText = this.quill.langText
-    const langText = ZH_CN
-    super(quill, resolveOptions(langText, options))
+    if (!options?.tipTextMap) {
+      options.tipTextMap = {}
+    }
+    super(quill, options)
   }
-}
-function resolveOptions(langText: Record<string, string>, options: Partial<QuillToolbarTipOptions>): Omit<QuillToolbarTipOptions, 'defaultTooltipOptions'> {
-  const resultOps = Object.assign({
-    tipTextMap: {},
-  }, options)
-  const btnBlotTips = [
-    'bold',
-    'italic',
-    'strike',
-    'underline',
-    'undo',
-    'redo',
-    'clean',
-    'link',
-    'blockquote',
-    'code',
-    'image',
-    'file',
-    'emoji',
-    'video',
-    'screenshot',
-    'fullscreen',
-    'better-table',
-    'code-block',
-    'formula',
-    'format-painter',
-  ].reduce((map, name) => {
-    map[name] = langText[name]
-    return map
-  }, {})
-  const selectTips = [
-    'list',
-    'script',
-    'indent',
-  ].reduce((map, name) => {
-    map[name] = {
-      onShow(_, value) {
-        return langText[`${name}-${value}`]
-      },
-    }
-    return map
-  }, {})
-  const simpleSelectTips = [
-    'color',
-    'background',
-    'font',
-    'size',
-  ].reduce((map, name) => {
-    map[name] = {
-      onShow() {
-        return langText[name]
-      },
-    }
-    return map
-  }, {})
 
-  resultOps.tipTextMap = {
-    ...btnBlotTips,
-    ...selectTips,
-    ...simpleSelectTips,
-    align: {
-      onShow(target, value) {
-        if (!value) value = 'left'
-        return langText[`align-${value}`] || null
+  resolveOptions(options: Partial<QuillToolbarTipOptions>): QuillToolbarTipOptions {
+    const result = super.resolveOptions(options)
+    const langText = this.quill.langText
+    const btnTips = [
+      'bold',
+      'italic',
+      'strike',
+      'underline',
+      'undo',
+      'redo',
+      'clean',
+      'link',
+      'blockquote',
+      'code',
+      'image',
+      'file',
+      'emoji',
+      'video',
+      'screenshot',
+      'fullscreen',
+      'better-table',
+      'code-block',
+      'formula',
+      'format-painter',
+    ].reduce((map, name) => {
+      map[name] = langText[name]
+      return map
+    }, {} as Record<string, string>)
+    const selectTips = [
+      'color',
+      'background',
+      'font',
+      'size',
+    ].reduce((map, name) => {
+      map[name] = {
+        onShow() {
+          return langText[name]
+        },
+      }
+      return map
+    }, {})
+    const valueControlTips = [
+      'list',
+      'align',
+      'script',
+      'indent',
+      'header',
+      'direction',
+    ].reduce((map, name) => {
+      map[name] = {
+        onShow(target: HTMLElement, value: string) {
+          if (name === 'direction') {
+            value = target.classList.contains('ql-active') ? 'rtl' : 'ltr'
+          }
+          if (!value) {
+            if (name === 'align') {
+              value = 'left'
+            }
+            else if (name === 'header') {
+              value = 'normal'
+            }
+          }
+          return langText[`${name}-${value}`]
+        },
+      }
+      return map
+    }, {})
+    const textMap: Record<string, QuillToolbarTipOptions['tipTextMap']> = {
+      ...btnTips,
+      ...valueControlTips,
+      ...selectTips,
+    }
+    return {
+      ...result,
+      tipTextMap: {
+        ...textMap,
+        ...options.tipTextMap,
       },
-    },
-    direction: {
-      onShow(target) {
-        return langText[target.classList.contains('ql-active') ? 'direction-ltr' : 'direction-rtl']
-      },
-    },
-    header: {
-      onShow(_, value) {
-        if (!value) value = 'normal'
-        return langText[`header-${value}`]
-      },
-    },
+    }
   }
-  return resultOps
 }
