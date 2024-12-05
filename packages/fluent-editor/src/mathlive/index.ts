d6d280f7908f04008958f61d723dd86c10609249
zzxming <74341337+zzxming@users.noreply.github.com>
2024-12-05 22:02:02 +0800

    fix: mathlive formula should not display edit dialog when quill disabled (#138)

--- packages/fluent-editor/src/mathlive/index.ts
+++ packages/fluent-editor/src/mathlive/index.ts
@@ -1,17 +1,33 @@
-import type Quill from 'quill'
-import { Module } from 'quill'
+import Quill from 'quill'
+import MathliveBlot from './formats'
 import MathliveTooltip from './tooltip'
 
-export default class MathliveModule extends Module<boolean> {
+export default class MathliveModule {
   tooltip: MathliveTooltip
-  constructor(quill: Quill, options?: boolean) {
-    super(quill, options)
-    // @ts-ignore
-    quill.root.quill = quill
+  constructor(public quill: Quill) {
     this.tooltip = new MathliveTooltip(quill)
+
+    this.quill.root.addEventListener(
+      'click',
+      (e: MouseEvent) => {
+        if (!this.quill.isEnabled()) return
+        const path = e.composedPath() as HTMLElement[]
+        if (!path || path.length <= 0) return
+
+        const mathliveNode = path.find(node => node.tagName && node.tagName.toUpperCase() === MathliveBlot.tagName.toUpperCase() && node.classList.contains(MathliveBlot.className))
+        const mathliveBlot = Quill.find(mathliveNode) as MathliveBlot | null
+        if (mathliveBlot) {
+          const { value, mode } = MathliveBlot.value(mathliveBlot.domNode)
+          if (mode === 'dialog') {
+            this.createDialog(value)
+          }
+        }
+      },
+      true,
+    )
   }
 
-  async createDialog(value?: string) {
+  createDialog(value?: string) {
     this.tooltip.edit(value)
   }
 }
