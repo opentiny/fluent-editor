a0416fae5bfb283c5c8b30849f30fa9b96bbace2
zzxming <zzxming77@163.com>
2024-12-08 17:06:55 +0800

    feat: header-list

--- packages/fluent-editor/src/utils/is.ts
+++ packages/fluent-editor/src/utils/is.ts
@@ -1,5 +1,7 @@
-export function isObject(value): value is object {
+export function isObject(value: unknown): value is object {
   const type = typeof value
   return value != null && (type === 'object' || type === 'function')
 }
 export const isUndefined = (val: unknown): val is undefined => val === undefined
+export const isFunction = (val: unknown): val is Function => typeof val === 'function'
+export const isString = (val: unknown): val is string => typeof val === 'string'
