6aa523b40481dbc87ef6f46ba92ee6f667c7e370
Kagol <kagol@sina.com>
2024-12-16 14:58:36 +0800

    chore: add fluent editor version (#153)

--- packages/docs/fluent-editor/.vitepress/theme/insert-deps-version.ts
+++ packages/docs/fluent-editor/.vitepress/theme/insert-deps-version.ts
@@ -1,3 +1,5 @@
+import packageJson from '@opentiny/fluent-editor/package.json'
+
 export function inertDepsVersion() {
   if (typeof document === 'undefined') return
 
@@ -7,9 +9,11 @@ export function inertDepsVersion() {
     npm_package_dependencies_vue: VueVersion,
     npm_package_dependencies_quill: QuillVersion,
   } = process.env
+  const FluentEditorVersion = packageJson.version
 
   document.body.setAttribute('data-vite-version', ViteVersion)
   document.body.setAttribute('data-vitepress-version', VitePressVersion)
   document.body.setAttribute('data-vue-version', VueVersion)
   document.body.setAttribute('data-quill-version', QuillVersion)
+  document.body.setAttribute('data-fluent-editor-version', FluentEditorVersion)
 }
