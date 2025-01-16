812bdd24502145cbc848aee6e32987f0bca07a1e
Kagol <kagol@sina.com>
2025-01-16 21:20:26 +0800

    docs: add translate

---
+++ packages/docs/fluent-editor/.vitepress/components/TranslateComponent.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+const currentLang = ref('chinese_simplified')
+
+onMounted(() => {
+  setTimeout(() => {
+    currentLang.value = translate.language.getCurrent()
+  }, 3000)
+})
+
+const switchLang = () => {
+  if (translate.language.getCurrent() === 'chinese_simplified') {
+    translate.changeLanguage('english')
+    currentLang.value = 'english'
+  } else {
+    translate.changeLanguage('chinese_simplified')
+    currentLang.value = 'chinese_simplified'
+  }
+}
+</script>
+
+<template>
+  <div class="translate-switch">
+    <a @click="switchLang">{{ currentLang === 'english' ? 'English' : '简体中文' }}</a>
+  </div>
+</template>
+
+<style scoped>
+.translate-switch {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  cursor: pointer;
+}
+</style>
