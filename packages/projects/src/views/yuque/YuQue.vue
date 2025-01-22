c0038594f43043d38cc29861064be8d5f6cf37ff
Kagol <kagol@sina.com>
2025-01-22 21:06:36 +0800

    docs: add projects (#216)
    * docs: add projects
    
    * docs(projects): add vue-router
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ packages/projects/src/views/yuque/YuQue.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FluentEditor from '@opentiny/fluent-editor'
+import { onMounted } from 'vue'
+
+let editor
+
+onMounted(() => {
+  editor = new FluentEditor('#editor', {
+    theme: 'snow',
+  })
+})
+</script>
+
+<template>
+  <RouterLink to="/">
+    &lt;返回
+  </RouterLink>
+  <div id="editor">
+    <p>Hello <strong>TinyEditor</strong>!</p>
+  </div>
+</template>
---
+++ packages/projects/vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+const fluentEditorRoot = path.resolve(__dirname, '../fluent-editor')
+export default defineConfig({
+  base: '/tiny-editor/projects/',
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@opentiny/fluent-editor/style.scss',
+        replacement: path.resolve(fluentEditorRoot, 'src/assets/style.scss'),
+      },
+    ],
+  },
+  build: {
+    outDir: '../docs/fluent-editor/.vitepress/dist/projects',
+  },
+})
