64817e9b4c520445f87a0b6e845ee44553338e89
Kagol <kagol@sina.com>
2025-01-23 21:17:36 +0800

    docs(projects): optimize imitate yuque (#218)
    * docs(projects): optimize imitate yuque
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/projects/src/views/home/Home.vue
+++ packages/projects/src/views/home/Home.vue
@@ -1,9 +1,11 @@
 <template>
-  <ul>
-    <li>
-      <RouterLink to="/yuque">
-        语雀
-      </RouterLink>
-    </li>
-  </ul>
+  <div class="flex justify-center m-8">
+    <ul class="list-disc">
+      <li>
+        <RouterLink to="/yuque">
+          仿语雀
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
